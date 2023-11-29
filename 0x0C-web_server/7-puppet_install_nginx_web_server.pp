0821c9bedacbd390cf49db47c6dcaf861f93a1cb
Shadkoech <shadrackipruto@gmail.com>
2023-11-29 14:50:52 +0300

    Configuring your Nginx server using puppet lint

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+# installation and configuration of the nginx server using puppet lint
+
+package { 'nginx':
+  ensure => installed,
+}
+
+#confirguring redirect
+file_line { 'redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://github.com/Shadkoech permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
