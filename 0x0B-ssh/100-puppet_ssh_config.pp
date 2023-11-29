32e4ae4d23a5b8c84b664a8f2655904f59b3f916
Shadkoech <shadrackipruto@gmail.com>
2023-11-29 22:36:27 +0300

    Puppet file update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Manifest to make changes to Confirguration file
 
-file { 'configured':
+file_line { 'configured':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
+  line   => '    IdentityFile ~/.ssh/school',
 }
 
-file { 'No password authentication':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
+file_line { 'No password authentication':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
 }
