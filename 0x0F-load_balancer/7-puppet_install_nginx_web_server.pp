0e74e884f8f96b449a1193ae13f6315039774896
Shadkoech <shadrackipruto@gmail.com>
2023-12-04 15:57:13 +0300

    Add a custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,31 @@
+# installation and configuration of the nginx server using puppet lint
+
+package { 'nginx':
+  ensure => installed,
+}
+
+#confirguring redirect
+file_line { 'redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://github.com/Shadkoech permanent;',
+}
+
+#Add custom HTTP header
+file_line { 'X-Served-By':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'rewrite ^/redirect_me https://github.com/Shadkoech permanent;',
+  line   => "add_header X-Served-By ${hostname};",
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+
