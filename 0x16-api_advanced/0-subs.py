2334f85f20814ae2bb0ab64223b551c43dc0afde
Shadkoech <shadrackipruto@gmail.com>
2024-01-16 14:08:41 +0300

    querying the Reddit API and return to 10 hot posts

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     # Setting up a custom User-Agent to avoid too many Request errors
     headers = {'User-Agent': 'Mozilla/5.0'}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     output = response.json()
 
     if response.status_code != 200:
