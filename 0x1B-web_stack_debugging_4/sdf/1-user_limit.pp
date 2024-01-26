c789769df3ab52af123002b2feacdede5884a1bc
Shadkoech <shadrackipruto@gmail.com>
2024-01-26 05:29:08 +0300

    Confirguring nginx to handle the pressure

---
+++ 0x1B-web_stack_debugging_4/sdf/1-user_limit.pp
@@ -0,0 +1,15 @@
+# script to enable holberton user to login and open files without error
+
+
+# Increase hard file limit for Holberton user.
+exec { 'increase-file-hard-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin:/bin/',
+}
+
+
+# Increase soft file limit for Holberton user.
+exec { 'increase file limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
